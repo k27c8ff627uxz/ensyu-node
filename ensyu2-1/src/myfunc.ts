4a4d9ba9545209e9ae24bd705d0f0bb07a135e30
MasahiroSato <k27c8ff627uxz@yahoo.co.jp>
2020-05-20 22:41:32 +0900

    結果をcallbackに

--- ensyu2-1/src/myfunc.ts
+++ ensyu2-1/src/myfunc.ts
@@ -4,6 +4,5 @@ interface Event { }
 
 export async function handler(event: Event, context: Context, callback: Callback) {
     const env = process.env['MY_ENV'];
-    console.log(`env is ${env}`)
-    callback(null, "A lambda is fiished.");
+    callback(null, `env is ${env}`);
 }
