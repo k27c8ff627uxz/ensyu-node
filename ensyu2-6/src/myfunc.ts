2151d32d3949b9b16ba3a73c0067a708d9dac593
root <k27c8ff627uxz@yahoo.co.jp>
2020-05-27 16:19:39 +0000

    Add ensyu2-6

---
+++ ensyu2-6/src/myfunc.ts
@@ -0,0 +1,9 @@
+import { Context, Callback } from "aws-lambda";
+
+// Run 'sls deploy [ --myopt hoge ]"
+interface Event { }
+
+export async function handler(event: Event, context: Context, callback: Callback) {
+    const myopt = process.env['MY_OPT'];
+    callback(null, `myopt: ${myopt}`);
+}
