4fd60d2c48cf944f8b0861b70361edb907b2ffa1
MasahiroSato <k27c8ff627uxz@yahoo.co.jp>
2020-06-07 16:23:10 +0900

    Add ensyu4-2

---
+++ chapter4/ensyu2/src/myfunc.ts
@@ -0,0 +1,15 @@
+import { Context, APIGatewayEvent, APIGatewayProxyCallback } from "aws-lambda";
+
+export function handler(event: APIGatewayEvent, context: Context, callback: APIGatewayProxyCallback) {
+    console.log(JSON.stringify(event));
+    callback(
+        null,
+        {
+            statusCode: 200,
+            headers: {
+                 "content-type": "text/html",
+            },
+            body: "Hello, World!"
+        }
+    )
+}
