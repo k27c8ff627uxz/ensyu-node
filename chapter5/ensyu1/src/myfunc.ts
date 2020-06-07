5e29c6d37bb2fdc288eeb23e54518a1ece6d7500
MasahiroSato <k27c8ff627uxz@yahoo.co.jp>
2020-06-07 17:37:17 +0900

    微修正

--- chapter5/ensyu1/src/myfunc.ts
+++ chapter5/ensyu1/src/myfunc.ts
@@ -2,6 +2,6 @@ import { Context, Callback, S3Event, S3EventRecord } from "aws-lambda";
 
 interface Event { }
 
-export function handler(event: Event, context: Context, callback: Callback) {
+export function handler(event: S3Event, context: Context, callback: Callback) {
     console.log(JSON.stringify(event));
 }
