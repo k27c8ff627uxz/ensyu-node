698a0bca9f000c643647964ab6dd17296563d0fc
MasahiroSato <k27c8ff627uxz@yahoo.co.jp>
2020-10-14 01:43:54 +0900

    Add nuxt_html_rendering

---
+++ nuxt_html_rendring/components/MyStrong.vue
@@ -0,0 +1,19 @@
+<template>
+    <span class="mystrong">
+        <slot />
+    </span>
+</template>
+
+<script lang='ts'>
+export default {
+    name: "my-strong"
+}
+</script>
+
+<style scoped>
+.mystrong {
+    margin: 0;
+    color: red;
+    font-weight: 700;
+}
+</style>
