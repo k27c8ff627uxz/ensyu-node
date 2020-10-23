a704b0411e67c891cf47ebd0db125cf342e8199f
MasahiroSato <k27c8ff627uxz@yahoo.co.jp>
2020-10-24 01:36:36 +0900

    Add react_style/ensyu2

---
+++ react_style/ensyu2/src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+ReactDOM.render(
+    <div className='mycolor'>
+        Hello, <span>React!!</span>
+    </div>,
+    document.getElementById('root')
+);
